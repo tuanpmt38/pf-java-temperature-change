428889b1a2c2b953879938a081696495072f0fae
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 13:42:02 +0700

    pf- java change C to F

--- TemperatureChange.java
+++ TemperatureChange.java
@@ -1,10 +1,10 @@
 public class TemperatureChange {
     public static void main(String[] args) {
-        float c, f;
+        float celsius, fahrenheit;
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.println("Nhập giá trị độ C");
-        c = sc.nextFloat();
-        f = (9 * c) / 5 + 32;//bieu thuc chuyen tu do C sang F
-        System.out.println("Độ F sau khi chuyển từ độ C là :" + f);
+        celsius = sc.nextFloat();
+        fahrenheit = (9 * celsius) / 5 + 32;//bieu thuc chuyen tu do C sang F
+        System.out.println("Độ F sau khi chuyển từ độ C là :" + fahrenheit);
     }
 }
